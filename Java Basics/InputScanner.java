0a6384b91d825c9d7d493440c46094d9008c7393
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2024-11-07 07:36:52 +0530

    Input in java - scanner

---
+++ Java Basics/InputScanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class InputScanner{
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+        scanner.close();
+    }
+}
