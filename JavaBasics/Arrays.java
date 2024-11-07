4b8764adc431596fa96100776a9d8fc37540fb4b
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2024-11-07 13:25:06 +0530

    Arrays

---
+++ JavaBasics/Arrays.java
@@ -0,0 +1,23 @@
+package JavaBasics;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        int[] squares = new int[5];
+
+        for(int i = 0; i < nums.length; i++){
+            squares[i] = nums[i] * nums[i];
+        }
+        System.out.print("Nums array is : ");
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Squares array is : ");
+        for(int i = 0; i < squares.length; i++){
+            System.out.print(squares[i] + " ");
+        }
+
+
+    }
+}
