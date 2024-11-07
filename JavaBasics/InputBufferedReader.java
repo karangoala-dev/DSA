e79ee19e7e54344c6a2902ecf3c02fb54f87dbe5
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2024-11-07 09:00:12 +0530

    Input in java - Buffered reader

--- JavaBasics/InputBufferedReader.java
+++ JavaBasics/InputBufferedReader.java
@@ -1,5 +1,16 @@
 package JavaBasics;
 
+import java.io.*;
+
 public class InputBufferedReader {
-    
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Enter you name please : ");
+            String name = reader.readLine();
+            System.out.println("Hello " + name);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
