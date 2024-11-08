3bf21dd01b04f6ec00dba74e2e95b73d8a2e87cf
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2024-11-08 09:01:28 +0530

    ArrayDeque

---
+++ Collections/ArrayDeques.java
@@ -0,0 +1,23 @@
+package Collections;
+
+import java.util.*;
+
+public class ArrayDeques {
+    public static void main(String[] args) {
+        ArrayDeque<String> adq = new ArrayDeque<>();
+
+        System.out.println("-------------------offer - add a new element----------------------");
+        adq.offer("Lion");
+        adq.offer("Dog");
+        adq.offer("Cat");
+        System.out.println(adq);
+        System.out.println("-------------------peek - view top element----------------------");
+        System.out.println(adq.peek());
+
+        System.out.println("-------------------poll - remove top element----------------------");
+        adq.poll();
+        System.out.println(adq);
+
+        System.out.println("-------------------offerFirst(), offerLast(), pollFirst(), pollLast(), peekFirst(), peekLast()----------------------");
+    }
+}
