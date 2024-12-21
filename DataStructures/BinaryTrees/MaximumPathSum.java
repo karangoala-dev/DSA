b9434ed5b59eb0bbca8371e24a7b8d1e9263151d
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2024-12-21 12:12:39 +0530

    MaximumPathSum.java implemented

--- DataStructures/BinaryTrees/MaximumPathSum.java
+++ DataStructures/BinaryTrees/MaximumPathSum.java
@@ -1,5 +1,16 @@
 package DataStructures.BinaryTrees;
 
+
+//Key Concepts:
+//Split vs. Unsplit Path:
+//
+//A split path includes the current node and both its left and right subtrees.
+//An unsplit path includes the current node and only one of its subtrees (left or right).
+//The helper function calculates both:
+//
+//res tracks the maximum split path globally.
+//        The return value of helper is the maximum unsplit path starting from the current node.
+
 public class MaximumPathSum {
     public int res = 0;
     public int helper(Node node){
