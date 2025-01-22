3677aa5ff2a1d32465611baba3b4bfbe63b1f762
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-01-22 19:11:19 +0530

    InfiniteArraySearch implemented

---
+++ Algorithms/BinarySearch/InfiniteArraySearch.java
@@ -0,0 +1,44 @@
+package Algorithms.BinarySearch;
+
+public class InfiniteArraySearch {
+    public static int binarySearch(int[] arr, int target, int l, int h){
+        while(l <= h){
+            int mid = l + ((h - l) / 2);
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            if(arr[mid] < target){
+                l = mid + 1;
+            }
+            else{
+                h = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static int searchInfiniteArray(int[] arr, int target){
+        int l = 0;
+        int h = 1;
+        while(arr[h] < target){
+            l = h;
+            h = 2 * h;
+        }
+        return binarySearch(arr, target, l, h);
+    }
+    public static void main(String[] args) {
+        // Test case: Simulated infinite array
+        int[] arr = {2, 3, 5, 7, 9, 14, 18, 21, 25, 30, 35, 40, 45, 50};
+        int target = 18;
+
+        // Call the searchInfiniteArray function
+        int result = searchInfiniteArray(arr, target);
+
+        // Print the result
+        if (result != -1) {
+            System.out.println("Target " + target + " found at index: " + result);
+        } else {
+            System.out.println("Target " + target + " not found in the array.");
+        }
+    }
+}
--- Algorithms/TwoPointers/ThreeSum.java
+++ Algorithms/TwoPointers/ThreeSum.java
@@ -1,5 +1,9 @@
 package Algorithms.TwoPointers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
