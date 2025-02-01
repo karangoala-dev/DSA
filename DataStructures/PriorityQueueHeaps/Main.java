aff4fd89aac0e6604d0b7ad807a66640277c35c1
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-02-01 22:53:30 +0530

    Heaps implemented

---
+++ DataStructures/PriorityQueueHeaps/Main.java
@@ -0,0 +1,4 @@
+package DataStructures.PriorityQueueHeaps;
+
+public class Main {
+}
---
+++ DataStructures/PriorityQueueHeaps/MinHeap.java
@@ -0,0 +1,24 @@
+package DataStructures.PriorityQueueHeaps;
+
+public class MinHeap {
+    private int[] arr;
+    public MinHeap(int[] a){
+        this.arr = a;
+    }
+
+    public void insert(){
+
+    }
+
+    public void getMin(){
+
+    }
+
+    public void extractMin(){
+
+    }
+
+    public void heapify(){
+
+    }
+}
