4d7b179a91f5172dfe1362bfb267c9c3fe1d7e61
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-02-07 23:11:26 +0530

    JumpGame2 implemented

--- Algorithms/Greedy/JumpGame2.java
+++ Algorithms/Greedy/JumpGame2.java
@@ -31,6 +31,22 @@ public class JumpGame2 {
         return helper(nums, 0, 0);
     }
 
+    //Greedy solution, TC -> O(N)
+    public int jump_greedy(int[] nums) {
+        int l = 0, r = 0, jumps = 0;
+        while(r < nums.length - 1){
+            //Find the farthest we can go from current position.
+            int farthest = 0;
+            for(int i = l; i <= r; i++){
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            //Now, update the left and right pointers wrt the farthest point
+            l = r + 1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
 
     public static void main(String[] args) {
         System.out.println(jump_recursive_cached(new int[]{5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5}));
