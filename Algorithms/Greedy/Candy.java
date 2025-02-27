62c8cf0d337aaccf68ddc26f5c49cd25f8a05e90
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-02-27 09:56:01 +0530

    Candy  implemented

--- Algorithms/Greedy/Candy.java
+++ Algorithms/Greedy/Candy.java
@@ -2,7 +2,7 @@ package Algorithms.Greedy;
 
 public class Candy {
     //Space not optimised
-    public int candy(int[] ratings) {
+    public int candy2NSpace(int[] ratings) {
         int res = 0;
         int[] left = new int[ratings.length];
         left[0] = 1;
@@ -26,21 +26,47 @@ public class Candy {
             }
         }
 
-        for(int i = 0; i < left.length; i++){
-            System.out.print(left[i]);
-        }
-        System.out.println();
-        for(int i = 0; i < left.length; i++){
-            System.out.print(right[i]);
-        }
-
         for(int i = 0; i < ratings.length; i++){
             res += Math.max(left[i], right[i]);
         }
 
         return res;
     }
-    public static void main(String[] args) {
 
+    public int candy1NSpace(int[] ratings) {
+        int res = 0;
+        int[] left = new int[ratings.length];
+        left[0] = 1;
+
+
+        for(int i=1; i < ratings.length; i++){
+            if(ratings[i] > ratings[i - 1]){
+                left[i] = left[i - 1] + 1;
+            }
+            else{
+                left[i] = 1;
+            }
+        }
+        int curr = 1, prev = 1;
+        res = res + Math.max(prev, left[left.length - 1]);
+        for(int i=ratings.length - 2; i >= 0; i--){
+            int current;
+            if(ratings[i] > ratings[i + 1]){
+                current = prev + 1;
+            }
+            else {
+                current = 1;
+            }
+            prev = current;
+            res += Math.max(current, left[i]);
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+        Candy candy = new Candy();
+        int[] arr = new int[]{1,2,87,87,87,2,1};
+        System.out.println(candy.candy1NSpace(arr));
+        System.out.println(candy.candy2NSpace(arr));
     }
 }
