c8166adc5263f787460235996730513c8cd43f48
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-02 13:27:03 +0530

    Graph  implemented

--- DataStructures/Graphs/DFS.java
+++ DataStructures/Graphs/DFS.java
@@ -18,7 +18,7 @@ public class DFS {
             return ;
         }
 
-        System.out.println(node);
+        System.out.print(node);
         isVisited[node] = 1;
 
         List<Integer> neighbours = graph.getNeighbors(node);
