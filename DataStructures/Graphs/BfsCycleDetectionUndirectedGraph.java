0786583524464176f0fa01fa89abdf9e4409df2a
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-08 13:27:01 +0530

    Trie  implemented

--- DataStructures/Graphs/BfsCycleDetectionUndirectedGraph.java
+++ DataStructures/Graphs/BfsCycleDetectionUndirectedGraph.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Node{
+class Node {
     int node;
     int prev;
 
-    Node(int n, int p){
+    Node(int n, int p) {
         this.node = n;
         this.prev = p;
     }
@@ -18,9 +18,9 @@ public class BfsCycleDetectionUndirectedGraph {
     public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
         int n = adj.size();
         int[] visited = new int[n];
-        for(int startNode = 0; startNode < n; startNode++){
-            if(visited[startNode] == 0){
-                if(bfs(adj, visited, startNode) == true) {
+        for (int startNode = 0; startNode < n; startNode++) {
+            if (visited[startNode] == 0) {
+                if (bfs(adj, visited, startNode) == true) {
                     return true;
                 }
             }
@@ -28,19 +28,19 @@ public class BfsCycleDetectionUndirectedGraph {
         return false;
     }
 
-    public boolean bfs(ArrayList<ArrayList<Integer>> adj, int[] visited, int start){
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj, int[] visited, int start) {
         visited[start] = 1;
         Queue<Node> q = new LinkedList<>();
         q.add(new Node(start, -1));
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Node current = q.poll();
             ArrayList<Integer> neighbours = adj.get(current.node);
-            for(int i = 0; i < neighbours.size(); i++){
-                if(visited[neighbours.get(i)] != 0 && neighbours.get(i) != current.prev){
+            for (int i = 0; i < neighbours.size(); i++) {
+                if (visited[neighbours.get(i)] != 0 && neighbours.get(i) != current.prev) {
                     return true;
                 }
 
-                if(visited[neighbours.get(i)] == 0 && neighbours.get(i) != current.prev){
+                if (visited[neighbours.get(i)] == 0 && neighbours.get(i) != current.prev) {
                     q.add(new Node(neighbours.get(i), current.node));
                     visited[neighbours.get(i)] = 1;
                 }
