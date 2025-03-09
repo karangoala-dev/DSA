c4752bc7f685d88d5ceaf08fc5e5dae201613a4a
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-09 21:04:51 +0530

    NumberOfIslands  implemented

--- DataStructures/Graphs/NoOfDistinctIslands.java
+++ DataStructures/Graphs/NoOfDistinctIslands.java
@@ -3,22 +3,24 @@ package DataStructures.Graphs;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-class IslandNode{
-    int r;
-    int c;
-    IslandNode(int _r, int _c){
-        this.r = _r;
-        this.c = _c;
-    }
-
-    public String toString(){
-        return Integer.toString(this.r) + " " + Integer.toString(this.c);
-    }
-}
-
 public class NoOfDistinctIslands {
-    void dfs(int[][] grid, int[][] visited, int r0, int c0, HashSet<ArrayList<String>> set){
+    void dfs(int[][] grid, int[][] visited, int r0, int c0, ArrayList<String> list, int r, int c){
+        int n = grid.length, m = grid[0].length;
+        list.add(toString(r-r0, c-c0));
+        int[] row = {0, -1, 0, 1};
+        int[] col = {-1, 0, 1, 0};
+        for(int i = 0; i < 4; i++){
+            int nRow = r + row[i];
+            int nCol = c + col[i];
+            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && visited[nRow][nCol] == 0 && grid[nRow][nCol] == 1){
+                visited[nRow][nCol] = 1;
+                dfs(grid, visited, r0, c0, list, nRow, nCol);
+            }
+        }
+    }
 
+    public String toString(int r, int c){
+        return Integer.toString(r) + " " + Integer.toString(c);
     }
 
     int countDistinctIslands(int[][] grid) {
@@ -28,10 +30,13 @@ public class NoOfDistinctIslands {
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(visited[i][j] == 0 && grid[i][j] == 1){
-                    dfs(grid, visited, i, j, set);
+                    ArrayList<String> list = new ArrayList<>();
+                    dfs(grid, visited, i, j, list, i, j);
+                    set.add(list);
                 }
             }
         }
+        return set.size();
     }
     public static void main(String[] args) {
 
