98febc05669c65e72c676db033c4ece789270fcc
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-10 14:33:39 +0530

    FloodFill  implemented

---
+++ DataStructures/Graphs/FloodFill.java
@@ -0,0 +1,14 @@
+package DataStructures.Graphs;
+
+import java.util.*;
+
+class Pixel{
+    int r;
+    int c;
+    Pixel(int _r, int _c){
+        this.r = _r;
+        this.c = _c;
+    }
+}
+public class FloodFill {
+}
