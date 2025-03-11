35e4c5cadd7f10711ebf5e8c4b7618d5d29f79c0
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-11 08:32:27 +0530

    BipartiteGraphDFS  implemented

---
+++ DataStructures/Graphs/BipartiteGraphDFS.java
@@ -0,0 +1,44 @@
+package DataStructures.Graphs;
+
+public class BipartiteGraphDFS {
+    public boolean dfs(int[][] graph, int start, int[] colored, int parentColor){
+        int n = graph.length;
+        if(parentColor == 0){
+            colored[start] = 1;
+        }
+        else if(parentColor == 1){
+            colored[start] = 2;
+        }
+        else if(parentColor == 2){
+            colored[start] = 1;
+        }
+
+        int[] neighbours = graph[start];
+        boolean res = true;
+        for(int i = 0; i < neighbours.length; i++){
+            if(colored[neighbours[i]] == 0){
+                res = res && dfs(graph, neighbours[i], colored, colored[start]);
+            }
+            else{
+                if(colored[neighbours[i]] == colored[start]){
+                    return false;
+                }
+            }
+        }
+        return res;
+    }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] colored = new int[n];
+        boolean res = true;
+        for(int i = 0; i < n; i++){
+            if(colored[i] == 0){
+                res = res && dfs(graph, i, colored, 0);
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
