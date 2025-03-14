00ce6fad3247cf06b53833f8b22e70ff3efc12c2
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-14 15:53:44 +0530

    ShortestPathDAG implemented

---
+++ DataStructures/Graphs/ShortestPathDAG.java
@@ -0,0 +1,7 @@
+package DataStructures.Graphs;
+
+//This uses topological sort(any, BFS/DFS) to gather the result. Return array of minimum distance weight from 0. (What if source is dynamic?)
+//Input: V = 4, E = 2, edges = [[0,1,2], [0,2,1]]
+//Output: [0, 2, 1, -1]
+public class ShortestPathDAG {
+}
