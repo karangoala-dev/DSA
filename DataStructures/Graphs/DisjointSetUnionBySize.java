51f6c3eaa0f4ac60f7fb35f1ea03da96f3bca94d
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-27 19:38:01 +0530

    DisjointSetUnionBySize implemented

---
+++ DataStructures/Graphs/DisjointSetUnionBySize.java
@@ -0,0 +1,22 @@
+package DataStructures.Graphs;
+
+import java.util.*;
+
+public class DisjointSetUnionBySize {
+    List<Integer> size;
+    List<Integer> parent;
+    DisjointSetUnionBySize(int n){
+        this.size = new ArrayList<>(n + 1);
+        this.parent = new ArrayList<>(n + 1);
+        for(int i = 0; i < n + 1; i++){
+
+        }
+    }
+    public int findParent(int node){
+        if(parent.get())
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
