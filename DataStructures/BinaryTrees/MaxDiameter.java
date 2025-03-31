4ebd426abcf9230db5d6afda8337ecca17513567
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-31 10:34:00 +0530

    CheapestFlightsWithinKStops_BellmanFord implemented

--- DataStructures/BinaryTrees/MaxDiameter.java
+++ DataStructures/BinaryTrees/MaxDiameter.java
@@ -1,21 +1,34 @@
 package DataStructures.BinaryTrees;
 
 public class MaxDiameter {
-    private int maxDiameter = 0;
+    //For this qn, we need to check 2 things.
+    //1.Max depth at each node
+    //2.Max diameter at each node
+    private int maxSoFar = Integer.MIN_VALUE;
     public int helper(Node node){
         if(node == null){
+            //If node is null return -1 to make the result in parent node's call = 0
+            maxSoFar = Math.max(maxSoFar, 0);
+            return -1;
+        }
+        if(node.left == null && node.right == null){
+            maxSoFar = Math.max(maxSoFar, 0);
             return 0;
         }
+        //Get left depth and right depth.
+        int leftDepth = 1 + helper(node.left);
+        int rightDepth = 1 + helper(node.right);
 
-        int leftHeight = helper(node.left);
-        int rightHeight = helper(node.right);
+        //MaxSoFar is the max diameter so far
+        maxSoFar = Math.max(maxSoFar, (leftDepth + rightDepth));
 
-        maxDiameter = Math.max(maxDiameter, leftHeight + rightHeight);
-        return 1 + Math.max(leftHeight, rightHeight);
+        //Here we return the max depth for this node.
+        return Math.max(leftDepth, rightDepth);
+
     }
     public int diameterOfBinaryTree(Node root) {
         helper(root);
-        return maxDiameter;
+        return maxSoFar;
     }
 
     public static void main(String[] args) {
