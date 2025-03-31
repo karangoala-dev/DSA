212e9a60da30131ef5b2a308286efc86905c79fa
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-03-31 13:56:39 +0530

    LowestCommonAncestor_Recursive implemented

--- DataStructures/BinarySearchTrees/LowestCommonAncestor.java
+++ DataStructures/BinarySearchTrees/LowestCommonAncestor.java
@@ -7,12 +7,15 @@ public class LowestCommonAncestor {
         Node curr = root;
         while(curr != null){
             if(p.val > curr.val && q.val > curr.val){
+                //If both values are strictly greater go right
                 curr = curr.right;
             }
             else if(p.val < curr.val && q.val < curr.val){
+                //If both values are strictly smaller, go left
                 curr = curr.left;
             }
             else{
+                //return curr for other cases where curr might be p or q or curr has a value between p and q
                 return curr;
             }
         }
