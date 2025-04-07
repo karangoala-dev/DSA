15be1ade34806aa56faefa887f337d2ffa5919ed
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-07 08:58:16 +0530

    PacificAtlanticWaterFlow implemented

--- DataStructures/BinaryTrees/LowestCommonAncestor.java
+++ DataStructures/BinaryTrees/LowestCommonAncestor.java
@@ -15,19 +15,16 @@ public class LowestCommonAncestor {
         if(root == null || root == p || root == q){
             return root;
         }
-
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if(left == null){
-            return right;
-        }
-        else if (right == null) {
-            return left;
-        }
-        else{
+        //if both are not null, return root as it means, left and right of root have one of each
+        if(left != null && right != null){
             return root;
         }
+
+        //else it means, one of the node on one side is the LCA
+        return left == null ? right : left;
     }
 
     public static void main(String[] args) {
