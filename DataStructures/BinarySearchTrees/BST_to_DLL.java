31ae132544655e92d6f93f4b0594b6d596b74ffe
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-12 09:47:24 +0530

    BST_to_DLL implemented

--- DataStructures/BinarySearchTrees/BST_to_DLL.java
+++ DataStructures/BinarySearchTrees/BST_to_DLL.java
@@ -3,38 +3,41 @@ package DataStructures.BinarySearchTrees;
 import com.sun.source.tree.Tree;
 
 public class BST_to_DLL {
-    private class TreeNode{
+    private class TreeNode {
         int val;
         TreeNode left, right;
-        TreeNode(int _v){
+
+        TreeNode(int _v) {
             this.val = _v;
             this.left = this.right = null;
         }
     }
 
-    private class DoublyListNode{
+    private class DoublyListNode {
         int val;
         DoublyListNode prev, next;
-        DoublyListNode(int _v){
+
+        DoublyListNode(int _v) {
             this.val = _v;
             this.prev = this.next = null;
         }
     }
 
-    private class Result{
+    private class Result {
         DoublyListNode first;
         DoublyListNode last;
-        Result(DoublyListNode f, DoublyListNode l){
+
+        Result(DoublyListNode f, DoublyListNode l) {
             this.first = f;
             this.last = l;
         }
     }
 
-    public Result helper(TreeNode node){
-        if(node == null){
+    public Result helper(TreeNode node) {
+        if (node == null) {
             return null;
         }
-        if(node.left == null && node.right == null){
+        if (node.left == null && node.right == null) {
             DoublyListNode dllNode = new DoublyListNode(node.val);
             return new Result(dllNode, dllNode);
         }
@@ -44,10 +47,9 @@ public class BST_to_DLL {
         Result leftList = helper(node.left);
 
         //if left list is null, set left list as having only current node as first and last;
-        if(leftList == null){
+        if (leftList == null) {
             leftList = new Result(curr, curr);
-        }
-        else {
+        } else {
             //connect left ka last to current node dll node.
             leftList.last.next = curr;
             curr.prev = leftList.last;
@@ -56,7 +58,7 @@ public class BST_to_DLL {
         }
 
         Result rightList = helper(node.right);
-        if(rightList != null){
+        if (rightList != null) {
             //connect left list ka last to right list ka first
             leftList.last.next = rightList.first;
             rightList.first.prev = leftList.last;
@@ -69,7 +71,32 @@ public class BST_to_DLL {
     }
 
     public DoublyListNode bstToDoublyList(TreeNode root) {
+        if(root == null){
+            return null;
+        }
         return helper(root).first;
+    }
+
+    //for printing DLL
+    private static void printDLL(DoublyListNode head) {
+        if (head == null) {
+            System.out.println("Empty list.");
+            return;
+        }
+        DoublyListNode tail = null;
+        System.out.print("Forward: ");
+        while (head != null) {
+            System.out.print(head.val + " ");
+            if (head.next == null) tail = head;
+            head = head.next;
+        }
+
+        System.out.print("\nBackward: ");
+        while (tail != null) {
+            System.out.print(tail.val + " ");
+            tail = tail.prev;
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -108,5 +135,52 @@ public class BST_to_DLL {
             System.out.print(tail.val + " ");
             tail = tail.prev;
         }
+
+        System.out.println("\n\n--- Test Case 2: Only root node ---");
+        TreeNode single = converter.new TreeNode(10);
+        DoublyListNode head2 = converter.bstToDoublyList(single);
+        printDLL(head2);
+
+        System.out.println("\n\n--- Test Case 3: Left-skewed tree ---");
+        //     3
+        //    /
+        //   2
+        //  /
+        // 1
+        TreeNode leftSkewed = converter.new TreeNode(3);
+        leftSkewed.left = converter.new TreeNode(2);
+        leftSkewed.left.left = converter.new TreeNode(1);
+        DoublyListNode head3 = converter.bstToDoublyList(leftSkewed);
+        printDLL(head3);
+
+        System.out.println("\n\n--- Test Case 4: Right-skewed tree ---");
+        // 1
+        //  \
+        //   2
+        //    \
+        //     3
+        TreeNode rightSkewed = converter.new TreeNode(1);
+        rightSkewed.right = converter.new TreeNode(2);
+        rightSkewed.right.right = converter.new TreeNode(3);
+        DoublyListNode head4 = converter.bstToDoublyList(rightSkewed);
+        printDLL(head4);
+
+        System.out.println("\n\n--- Test Case 5: Null tree ---");
+        TreeNode nullTree = null;
+        DoublyListNode head5 = converter.bstToDoublyList(nullTree);
+        printDLL(head5);
+
+        System.out.println("\n\n--- Test Case 6: Unbalanced BST ---");
+        //       10
+        //      /
+        //     5
+        //      \
+        //       7
+        TreeNode unbalanced = converter.new TreeNode(10);
+        unbalanced.left = converter.new TreeNode(5);
+        unbalanced.left.right = converter.new TreeNode(7);
+        DoublyListNode head6 = converter.bstToDoublyList(unbalanced);
+        printDLL(head6);
+
     }
 }
