30406f964bc46a255a85042abc008fa2ee9d9321
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-15 09:07:53 +0530

    CountNegativeNos implemented

--- Algorithms/BinarySearch/SearchInRotatedSortedArray.java
+++ Algorithms/BinarySearch/SearchInRotatedSortedArray.java
@@ -1,73 +1,55 @@
 package Algorithms.BinarySearch;
 
-//There is an integer array nums sorted in ascending order (with distinct values).
-//Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array
-// is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and
-// become [4,5,6,7,0,1,2].
-//Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
-//You must write an algorithm with O(log n) runtime complexity.
-//
-//Example 1:
-//Input: nums = [4,5,6,7,0,1,2], target = 0
-//Output: 4
-
-//Example 2:
-//Input: nums = [4,5,6,7,0,1,2], target = 3
-//Output: -1
-
-//Example 3:
-//Input: nums = [1], target = 0
-//Output: -1
-
-class Solution {
-    public int search(int[] arr, int t) {
-        int l = 0, r = arr.length - 1;
-        while(l <= r){
-            int m = (l + r) / 2;
-            boolean left = arr[m] >= arr[l]; //Check if we are on left sorted array or right sorted array
-            if(arr[m] == t){
-                return m;
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int l = 0, h = nums.length - 1;
+        while(l <= h){
+            int mid = (l + (h - l) / 2);
+            if(nums[mid] == target){
+                return mid;
             }
-            if(left){
-                if(t > arr[m]){
-                    l = m + 1;
-                }
-                else{
-                    if(t < arr[l]){
-                        l = m + 1;
+            if(target < nums[mid]){
+                //target smaller than mid value
+                if(nums[l] <= nums[mid]){
+                    //means we are on left side sorted portion
+                    if(target < nums[l]){
+                        //go right
+                        l = mid + 1;
                     }
                     else{
-                        r = m - 1;
+                        //go left
+                        h = mid - 1;
                     }
+                }
+                else{
+                    //means we are on right side of the sorted rotated portion
+                    //here for a smaller value than mid, only one option is correct
+                    //target must be on left
+                    h = mid - 1;
                 }
             }
             else{
-                if(t < arr[m]){
-                    r = m - 1;
+                //target greater than mid value
+                if(nums[l] <= nums[mid]){
+                    //if we are on left side go right
+                    l = mid + 1;
                 }
                 else{
-                    if(t > arr[r]){
-                        r = m - 1;
+                    //if we are on right side then
+                    if(target > nums[h]){
+                        //go left
+                        h = mid - 1;
                     }
                     else{
-                        l = m + 1;
+                        //go right
+                        l = mid + 1;
                     }
                 }
             }
         }
         return -1;
     }
-}
-
-public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] arr1 = {4, 5, 6, 7, 0, 1, 2};
-        int target1 = 0;
-        System.out.println("Index of " + target1 + ": " + solution.search(arr1, target1)); // Expected: 4
 
-        int[] arr2 = {4, 5, 6, 7, 0, 1, 2};
-        int target2 = 3;
-        System.out.println("Index of " + target2 + ": " + solution.search(arr2, target2)); // Expected: -1
     }
 }
