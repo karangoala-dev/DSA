24ca08355f0f5364e18d09f4fdc20a6d7114f639
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-21 09:35:29 +0530

    Permutations.java implemented

--- DataStructures/Graphs/SurroundedRegions_Optimised.java
+++ DataStructures/Graphs/SurroundedRegions_Optimised.java
@@ -1,40 +1,55 @@
 package DataStructures.Graphs;
 
 public class SurroundedRegions_Optimised {
-    public void dfs(char[][] board, int[][] visited, int sr, int sc){
+    //basic dfs fn to mark all connected O's as #
+    public void dfs(char[][] board, int r, int c){
         int n = board.length, m = board[0].length;
-        board[sr][sc] = '#';
-        visited[sr][sc] = 1;
         int[] row = {0, -1, 0, 1};
         int[] col = {-1, 0, 1, 0};
         for(int i = 0; i < 4; i++){
-            int nRow = sr + row[i];
-            int nCol = sc + col[i];
-            if(nRow < n && nRow >= 0 && nCol >= 0 && nCol < m && visited[nRow][nCol] == 0 && board[nRow][nCol] == 'O'){
-                dfs(board, visited, nRow, nCol);
+            int nR = row[i] + r;
+            int nC = col[i] + c;
+            if(nR >= 0 && nC >= 0 && nR < n && nC < m && board[nR][nC] == 'O'){
+                board[nR][nC] = '#';
+                dfs(board, nR, nC);
             }
         }
     }
     public void solve(char[][] board) {
         int n = board.length, m = board[0].length;
-        int[][] visited = new int[n][m];
+        //List of array where each arr is [r,c]
+        List<int[]> boundaryOs = new ArrayList<>();
+        //first row - last row
         for(int i = 0; i < m; i++){
-            if(board[0][i] == 'O' && visited[0][i] == 0){
-                dfs(board, visited, 0, i);
+            if(board[0][i] == 'O'){
+                board[0][i] = '#';
+                boundaryOs.add(new int[]{0,i});
             }
-            if(board[n - 1][i] == 'O' && visited[n - 1][i] == 0){
-                dfs(board, visited, n - 1, i);
-            }
-        }
-        for(int i = 0; i < n; i++){
-            if(board[i][0] == 'O' && visited[i][0] == 0){
-                dfs(board, visited, i, 0);
-            }
-            if(board[i][m - 1] == 'O' && visited[i][m - 1] == 0){
-                dfs(board, visited, i, m - 1);
+            if(board[n - 1][i] == 'O'){
+                board[n - 1][i] = '#';
+                boundaryOs.add(new int[]{n - 1,i});
             }
         }
 
+        //first col - last col
+        for(int i = 0; i < n; i++){
+            if(board[i][0] == 'O'){
+                board[i][0] = '#';
+                boundaryOs.add(new int[]{i, 0});
+            }
+            if(board[i][m - 1] == 'O'){
+                board[i][m - 1] = '#';
+                boundaryOs.add(new int[]{i, m - 1});
+            }
+        }
+
+        //for each boundary O, run a dfs
+        for(int[] node : boundaryOs){
+            dfs(board, node[0], node[1]);
+        }
+
+        //now only non-boundary connected O regions remain
+        //so mark all as X
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == 'O'){
@@ -42,6 +57,8 @@ public class SurroundedRegions_Optimised {
                 }
             }
         }
+
+        //mark back all #'s as O
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == '#'){
