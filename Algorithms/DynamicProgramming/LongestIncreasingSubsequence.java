31a32513be65f8378104f56dde7964bc07bc9a16
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-27 08:04:21 +0530

    LongestIncreasingSubsequence.java implemented

--- Algorithms/DynamicProgramming/LongestIncreasingSubsequence.java
+++ Algorithms/DynamicProgramming/LongestIncreasingSubsequence.java
@@ -1,7 +1,55 @@
 package Algorithms.DynamicProgramming;
 
-public class LongestIncreasingSubsequence {
+import java.util.Arrays;
 
+public class LongestIncreasingSubsequence {
+    int[][] dp;
+    public int helper(int[] nums, int ind, int prevInd){
+        if(ind >= nums.length){
+            //return 0 if we reached end
+            return 0;
+        }
+
+        //memoization
+        if(dp[ind][prevInd + 1] != -1){
+            return dp[ind][prevInd + 1];
+        }
+
+        //get the previous value
+        int prev;
+        int memoizedInd;
+        if(prevInd == -1){
+            prev = -10001;
+        }
+        else{
+            prev = nums[prevInd];
+        }
+
+        //take
+        int res1 = Integer.MIN_VALUE;
+        if(nums[ind] > prev){
+            res1 = 1 + helper(nums, ind + 1, ind);
+        }
+
+        //not-take
+        int res2 = helper(nums, ind + 1, prevInd);
+
+        return dp[ind][prevInd + 1] = Math.max(res1, res2);
+    }
+    public int lengthOfLIS(int[] nums) {
+        //dp is 2D
+        //1. row means index
+        //2. col means index of previous value in nums array(it could've been prev value, but that would have large no of unused space), + 1 is done to store initial -1 col
+        //dp[ind][prevInd + 1] = The maximum subsequence length you can achieve starting at index ind, given that your last taken number was at prevInd.The maximum subsequence length
+        // you can achieve starting at index ind, given that your last taken number was at prevInd.
+        dp = new int[nums.length][nums.length + 1];
+        for(int[] curr: dp){
+            Arrays.fill(curr, -1);
+        }
+
+        //initially we pass -1 as the index for previous
+        return helper(nums, 0, -1);
+    }
     public static void main(String[] args) {
 
     }
