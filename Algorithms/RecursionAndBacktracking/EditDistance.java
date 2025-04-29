ae6235cd9f5aa7766a153088e589c68afc6f8410
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-04-29 21:56:35 +0530

    LongestCommonSubsequence implemented

---
+++ Algorithms/RecursionAndBacktracking/EditDistance.java
@@ -0,0 +1,42 @@
+package Algorithms.RecursionAndBacktracking;
+
+public class EditDistance {
+    public int helper(String word1, String word2, int p1, int p2){
+        if(p1 == word1.length() || p2 == word2.length()){
+            if(p1 != word1.length()){
+                //if p1 is not at end, return remaining length
+                return word1.length() - p1;
+            }
+            else if(p2 != word2.length()){
+                return word2.length() - p2;
+            }
+            else{
+                //means both are at end so return 0
+                return 0;
+            }
+        }
+
+        if(word1.charAt(p1) == word2.charAt(p2)){
+            return helper(word1, word2, p1 + 1, p2 + 1);
+        }
+        else {
+            //insert a char in word1, which will be same as p2, so advance
+            int res1 = 1 + helper(word1, word2, p1, p2 + 1);
+
+            //delete char at p1, so do p1+1
+            int res2 = 1 + helper(word1, word2, p1 + 1, p2);
+
+            //replace char so advance both p1 and p2
+            int res3 = 1 + helper(word1, word2, p1 + 1, p2 + 1);
+
+            return Math.min(res1, Math.min(res2, res3));
+        }
+    }
+    public int minDistance(String word1, String word2) {
+        return helper(word1, word2, 0, 0);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
