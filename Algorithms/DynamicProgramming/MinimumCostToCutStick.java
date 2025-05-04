3d8a9d1f19fe79fcf648e661daf5360798b0e945
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-04 22:22:55 +0530

    MinimumCostToCutStick implemented

--- Algorithms/DynamicProgramming/MinimumCostToCutStick.java
+++ Algorithms/DynamicProgramming/MinimumCostToCutStick.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MinimumCostToCutStick {
     int[][] dp;
-    //this fn returns minimum cost to cut stick from index i to j
+    // This function returns the minimum cost to cut the stick between cuts[i] and cuts[j]
     public int helper(int[] cuts, int i, int j){
         if(i + 1 == j){
             //means no cuts can be done further, return 0
@@ -18,7 +18,11 @@ public class MinimumCostToCutStick {
         int min = Integer.MAX_VALUE;
 
         //for k in range from i + 1 to j - 1... ignore i and j as they are already cut
+        // Using this for loop, we are considering each possible cut (k) between cuts[i] and cuts[j]
+        // For each k, we recursively compute the cost of cutting the left (i to k) and right (k to j) segments
         for(int k = i + 1; k < j; k++){
+
+            // Cost = length of current stick + cost of remaining cuts on left part + cost of remaining cuts on right part
             int curr = cuts[j] - cuts[i] + helper(cuts, i, k) + helper(cuts, k, j);
 
             min = Math.min(min, curr);
@@ -27,10 +31,6 @@ public class MinimumCostToCutStick {
         return dp[i][j] = min;
     }
     public int minCost(int n, int[] cuts) {
-        dp = new int[cuts.length + 2][cuts.length + 2];
-        for(int[] curr: dp){
-            Arrays.fill(curr, -1);
-        }
         //+2 is done to add 0 and n to represent the stick
         int[] arr = new int[cuts.length + 2];
         arr[0] = 0;
@@ -39,6 +39,13 @@ public class MinimumCostToCutStick {
             arr[i + 1] = cuts[i];
         }
         Arrays.sort(arr);
+
+        //dp table should have a length same as cuts + 2
+        dp = new int[cuts.length + 2][cuts.length + 2];
+        for(int[] curr: dp){
+            Arrays.fill(curr, -1);
+        }
+
         return helper(arr, 0, arr.length - 1);
     }
     public static void main(String[] args) {
