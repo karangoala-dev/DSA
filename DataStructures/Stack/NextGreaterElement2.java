51aba05986c0c1be2418fa65a5fee154dac80ea2
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-10 12:12:12 +0530

    NextGreaterElement2 implemented

--- DataStructures/Stack/NextGreaterElement2.java
+++ DataStructures/Stack/NextGreaterElement2.java
@@ -36,6 +36,29 @@ public class NextGreaterElement2 {
 
         return res;
     }
+
+    //Optimal approach
+    public int[] nextGreaterElements_optimal(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 2 * n - 1; i >= 0; i--){
+            int ind = i % n;
+            while(!st.isEmpty() && st.peek() <= nums[ind]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                res[ind] = -1;
+            }
+            else{
+                res[ind] = st.peek();
+            }
+            st.push(nums[ind]);
+        }
+
+        return res;
+    }
     public static void main(String[] args) {
 
     }
