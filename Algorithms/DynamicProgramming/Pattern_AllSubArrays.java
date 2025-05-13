5ee7c5762243552f06a1e1aca267ffaae615dad9
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-13 14:27:40 +0530

    MinimumCostToMergeStones implemented

--- Algorithms/DynamicProgramming/Pattern_AllSubArrays.java
+++ Algorithms/DynamicProgramming/Pattern_AllSubArrays.java
@@ -4,24 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pattern_AllSubArrays {
-    public static void helper(int[] arr, int ind, List<Integer> curr){
-        if(ind == arr.length){
-            //means we reached end, so print subarray
-            System.out.println(curr);
+    public static void helper_iterative(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            List<Integer> curr = new ArrayList<>();
+            for(int j = i; j < arr.length; j++){
+                curr.add(arr[j]);
+                System.out.println(curr);
+            }
+        }
+    }
+
+    public static void helper_recursive(int[] arr, int i, int j, List<Integer> curr){
+        if(j == arr.length){
+            //end reached, exit
             return;
         }
 
-        //take
-        int prevSize = curr.size();
-        curr.add(arr[ind]);
-        helper(arr, ind + 1, curr);
-        curr.remove(prevSize);
+        //add current end index to curr and print as it is a new subarray
+        curr.add(arr[j]);
+        System.out.println(curr);
+        //recurse further using this subarray
+        helper_recursive(arr, i, j + 1, curr);
+        //backtrack
+        curr.remove(curr.size() - 1);
 
-        //not-take
-        helper(arr, ind + 1, curr);
+        //now, if end is reached, move to next i
+        if(i == j){
+            helper_recursive(arr, i + 1, i + 1, new ArrayList<>());
+        }
     }
     public static void main(String[] args) {
         int[] arr = {3,1,2};
-        helper(arr, 0, new ArrayList<>());
+        helper_recursive(arr, 0, 0, new ArrayList<>());
     }
 }
