f1acaae719b14dfbc0ff3750469ac259ad54667a
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-19 19:24:02 +0530

    SlidingWindowMaximum implemented

--- DataStructures/Stack/Dequeue/SlidingWindowMaximum.java
+++ DataStructures/Stack/Dequeue/SlidingWindowMaximum.java
@@ -2,7 +2,12 @@ package DataStructures.Stack.Dequeue;
 
 public class SlidingWindowMaximum {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+
+
+
+        return res;
     }
     public static void main(String[] args) {
 
