23e634cd9f3b645d3a61bbe729407f49dacb64f8
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-24 15:10:05 +0530

    MinimumSizeSubarraySum implemented

--- Algorithms/SlidingWindow/MinimumSizeSubarraySum.java
+++ Algorithms/SlidingWindow/MinimumSizeSubarraySum.java
@@ -1,6 +1,7 @@
 package Algorithms.SlidingWindow;
 
 public class MinimumSizeSubarraySum {
+    //using sliding window -> TC O(n)
     public int minSubArrayLen(int target, int[] nums) {
         int n = nums.length;
         int i = 0, j = 0, res = Integer.MAX_VALUE, currSum = 0;
@@ -13,6 +14,53 @@ public class MinimumSizeSubarraySum {
                 i++;
             }
             j++;
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    //using prefix sum -> TC O(n log n)
+    public int binarySearch(int[] arr, int l, int h, int diff_target){
+        //we need to find a arr[i] such that arr[i] <= diff_target
+        int ans = -1;
+        while(l <= h){
+            int mid = l + ((h - l) / 2);
+            if(arr[mid] <= diff_target){
+                //store potential ans
+                ans = mid;
+                //go right to find a larger index where condition holds
+                l = mid + 1;
+            }
+            else{
+                //go left since arr[mid] > diff_target
+                h = mid - 1;
+            }
+        }
+        return ans;
+    }
+    public int minSubArrayLen_prefixSum(int target, int[] nums) {
+        int n = nums.length;
+        int[] prefixSum = new int[n];
+        int res =Integer.MAX_VALUE, currSum = 0;
+        for(int j = 0; j < n; j++){
+            currSum += nums[j];
+            prefixSum[j] = currSum;
+
+            if(prefixSum[j] - target >= 0){
+                //means we may have some value prefixSum[i] such that
+                //prefixSum[j] - prefixSum[i] >= target
+                //i.e. prefixSum[i] <= prefixSum[j] - target
+                int ind = binarySearch(prefixSum, 0, j - 1, prefixSum[j] - target);
+
+                if (ind != -1) {
+                    //if we have found some index such that prefixSum[i] <= diff_target
+                    //so consider it for res
+                    res = Math.min(res, j - ind);
+                } else if (prefixSum[j] >= target) {
+                    //means 0->j can be an ans
+                    res = Math.min(res, j + 1);
+                }
+
+            }
         }
         return res == Integer.MAX_VALUE ? 0 : res;
     }
