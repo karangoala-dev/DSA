44224d7db09dd0ceeab3637eef5d3d4b9e00160a
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-31 08:35:46 +0530

    Permutations.java implemented

--- Algorithms/RecursionAndBacktracking/Permutations.java
+++ Algorithms/RecursionAndBacktracking/Permutations.java
@@ -3,28 +3,31 @@ package Algorithms.RecursionAndBacktracking;
 import java.util.*;
 
 public class Permutations {
-    List<List<Integer>> res = new ArrayList<>();
-    public void helper(List<Integer> nums, List<Integer> curr){
-        if(nums.size() == curr.size()){
+    List<List<Integer>> res;
+    HashSet<Integer> set;
+    public void helper(int[] nums, List<Integer> curr){
+        if(curr.size() == nums.length){
             res.add(new ArrayList<>(curr));
             return;
         }
-        for(int i = 0; i < nums.size(); i++){
-            if(curr.contains(nums.get(i))){
-                //skip this element is already considered in current list
+
+        for(int i = 0; i < nums.length; i++){
+            //consider ith element only if not present in curr
+            if(set.contains(nums[i])){
                 continue;
             }
-            curr.add(nums.get(i));
+            curr.add(nums[i]);
+            set.add(nums[i]);
             helper(nums, curr);
+
             curr.remove(curr.size() - 1);
+            set.remove(nums[i]);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<Integer> input = new ArrayList<>();
-        for(int num: nums){
-            input.add(num);
-        }
-        helper(input, new ArrayList<>());
+        res = new ArrayList<>();
+        set = new HashSet<>();
+        helper(nums, new ArrayList<>());
         return res;
     }
     public static void main(String[] args) {
