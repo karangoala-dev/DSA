9de57f09d39399d498258c279877797ab454f756
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-31 13:45:21 +0530

    Permutations.java implemented

--- Revision/CombinationSum2.java
+++ Revision/CombinationSum2.java
@@ -4,12 +4,33 @@ import java.util.*;
 
 public class CombinationSum2 {
     List<List<Integer>> res = new ArrayList<>();
+    HashSet<String> set = new HashSet<>();
 
-    public void helper(){
+    public void helper(int[] nums, int target, int ind, int currSum, List<Integer> curr){
+        if(ind >= nums.length || currSum >= target){
+            List<Integer> dummy = new ArrayList<>(curr);
+            Collections.sort(dummy);
+            String s = dummy.toString();
+            if(currSum == target && ! set.contains(s)){
+                set.add(s);
+                res.add(new ArrayList<>(curr));
+            }
+            return;
+        }
 
+        //take and move ahead
+        currSum += nums[ind];
+        curr.add(nums[ind]);
+        helper(nums, target, ind + 1, currSum, curr);
+
+        //backtrack, not take and move ahead
+        currSum -= nums[ind];
+        curr.remove(curr.size() - 1);
+        helper(nums, target, ind + 1, currSum, curr);
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        helper(candidates, target, 0, 0, new ArrayList<>());
         return res;
     }
     public static void main(String[] args) {
--- Revision/Permutations2.java
+++ Revision/Permutations2.java
@@ -5,35 +5,69 @@ import Algorithms.RecursionAndBacktracking.Permutations;
 import java.util.*;
 
 public class Permutations2 {
-    List<List<Integer>> res = new ArrayList<>();
-    HashSet<String> set = new HashSet<>();
-    HashSet<Integer> currentSet = new HashSet<>();
+    //NOOB approach
+//    List<List<Integer>> res = new ArrayList<>();
+//    HashSet<String> set = new HashSet<>();
+//    HashSet<Integer> currentSet = new HashSet<>();
+//
+//    public void helper(int[] nums, List<Integer> curr){
+//        if(curr.size() == nums.length){
+//            String s = curr.toString();
+//            if(!set.contains(s)){
+//                res.add(new ArrayList<>(curr));
+//                set.add(s);
+//                return;
+//            }
+//        }
+//
+//        for(int i = 0; i < nums.length; i++){
+//            if(currentSet.contains(i)){
+//                continue;
+//            }
+//            curr.add(nums[i]);
+//            currentSet.add(i);
+//            helper(nums, curr);
+//
+//            curr.remove(curr.size() - 1);
+//            currentSet.remove(i);
+//        }
+//    }
+//
+//    public List<List<Integer>> permuteUnique(int[] nums) {
+//        helper(nums, new ArrayList<>());
+//        return res;
+//    }
 
-    public void helper(int[] nums, List<Integer> curr){
+    //Efficient approach
+    List<List<Integer>> res = new ArrayList<>();
+    public void helper(int[] nums, List<Integer> curr, HashMap<Integer, Integer> map){
         if(curr.size() == nums.length){
-            String s = curr.toString();
-            if(!set.contains(s)){
-                res.add(new ArrayList<>(curr));
-                set.add(s);
-                return;
-            }
+            res.add(new ArrayList<>(curr));
+            return;
         }
 
-        for(int i = 0; i < nums.length; i++){
-            if(currentSet.contains(i)){
-                continue;
-            }
-            curr.add(nums[i]);
-            currentSet.add(i);
-            helper(nums, curr);
+        //for each entry, if count is > 0 then add to current list and continue
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            if(entry.getValue() > 0){
+                //take and recurse
+                curr.add(entry.getKey());
+                entry.setValue(entry.getValue() - 1);
+                helper(nums, curr, map);
 
-            curr.remove(curr.size() - 1);
-            currentSet.remove(i);
+                //backtrack
+                curr.remove(curr.size() - 1);
+                entry.setValue(entry.getValue() + 1);
+            }
         }
     }
-
     public List<List<Integer>> permuteUnique(int[] nums) {
-        helper(nums, new ArrayList<>());
+        //store element and frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int e : nums){
+            map.put(e, map.getOrDefault(e, 0) + 1);
+        }
+        helper(nums, new ArrayList<>(), map);
+
         return res;
     }
     public static void main(String[] args) {
