e76b2dc2e243983699a3017bfd466be73b23c759
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-05-31 15:05:34 +0530

    Permutations.java implemented

--- Revision/CombinationSum2.java
+++ Revision/CombinationSum2.java
@@ -4,33 +4,28 @@ import java.util.*;
 
 public class CombinationSum2 {
     List<List<Integer>> res = new ArrayList<>();
-    HashSet<String> set = new HashSet<>();
-
-    public void helper(int[] nums, int target, int ind, int currSum, List<Integer> curr){
+    public void helper(int[] nums, int target, int ind, List<Integer> curr, int currSum){
         if(ind >= nums.length || currSum >= target){
-            List<Integer> dummy = new ArrayList<>(curr);
-            Collections.sort(dummy);
-            String s = dummy.toString();
-            if(currSum == target && ! set.contains(s)){
-                set.add(s);
+            if(currSum == target){
                 res.add(new ArrayList<>(curr));
             }
             return;
         }
-
-        //take and move ahead
-        currSum += nums[ind];
+        //take and move to next
         curr.add(nums[ind]);
-        helper(nums, target, ind + 1, currSum, curr);
+        helper(nums, target, ind + 1, curr, currSum + nums[ind]);
 
-        //backtrack, not take and move ahead
-        currSum -= nums[ind];
+        //skip dupes
+        while(ind < nums.length - 1 && nums[ind] == nums[ind + 1]){
+            ind++;
+        }
+        //now backtrack and recurse further
         curr.remove(curr.size() - 1);
-        helper(nums, target, ind + 1, currSum, curr);
+        helper(nums, target, ind + 1, curr, currSum);
     }
-
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        helper(candidates, target, 0, 0, new ArrayList<>());
+        Arrays.sort(candidates);
+        helper(candidates, target, 0, new ArrayList<>(), 0);
         return res;
     }
     public static void main(String[] args) {
