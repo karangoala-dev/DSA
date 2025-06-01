400673bbc4f30f5cc961d1dbf5e81b5ce99299e1
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-06-01 09:07:04 +0530

    Permutations.java implemented

---
+++ Revision/utils/TreeNode.java
@@ -0,0 +1,17 @@
+package Revision.utils;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int _v){
+        this.val = _v;
+    }
+
+    public TreeNode(int _v, TreeNode _l, TreeNode _r){
+        this.val = _v;
+        this.left = _l;
+        this.right = _r;
+    }
+}
