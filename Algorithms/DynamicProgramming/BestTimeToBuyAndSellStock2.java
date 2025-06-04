5d1e64b95e15452156e71944cb8dceef81d07500
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-06-04 21:46:01 +0530

    BestTimeToBuyAndSellStock1 implemented

---
+++ Algorithms/DynamicProgramming/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,22 @@
+package Algorithms.DynamicProgramming;
+
+public class BestTimeToBuyAndSellStock2 {
+    public int helper(int[] prices, Boolean buy, int ind){
+        if(ind >= prices.length){
+            return 0;
+        }
+
+        if(buy){
+
+        }
+        else{
+
+        }
+    }
+    public int maxProfit(int[] prices) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
