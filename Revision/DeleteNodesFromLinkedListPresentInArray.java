388195f5c92eba83e4619c6a6d74614ea2396b03
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-06-09 19:36:38 +0530

    DeleteNodesFromLinkedListPresentInArray implemented

--- Revision/DeleteNodesFromLinkedListPresentInArray.java
+++ Revision/DeleteNodesFromLinkedListPresentInArray.java
@@ -2,10 +2,33 @@ package Revision;
 
 import Revision.utils.*;
 
+import java.util.HashSet;
+
 public class DeleteNodesFromLinkedListPresentInArray {
     ListNode dummy;
+    HashSet<Integer> set;
     public ListNode modifiedList(int[] nums, ListNode head) {
-        dummy = new ListNode();
+        dummy = new ListNode(Integer.MIN_VALUE);
+        set = new HashSet<>();
+        dummy.next = head;
+        for(int i: nums){
+            set.add(i);
+        }
+
+        ListNode prev = dummy;
+        ListNode curr = dummy.next;
+        while(curr != null){
+            if(set.contains(curr.val)){
+                //delete
+                prev.next = curr.next;
+            }
+            else {
+                prev = prev.next;
+            }
+            curr = curr.next;
+        }
+
+        return dummy.next;
     }
     public static void main(String[] args) {
 
