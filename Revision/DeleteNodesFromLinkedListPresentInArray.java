06ceb018be065690865d844eca9f171e164acd07
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-06-09 19:24:35 +0530

    NextPermutation implemented

---
+++ Revision/DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,13 @@
+package Revision;
+
+import Revision.utils.*;
+
+public class DeleteNodesFromLinkedListPresentInArray {
+    ListNode dummy;
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        dummy = new ListNode();
+    }
+    public static void main(String[] args) {
+
+    }
+}
