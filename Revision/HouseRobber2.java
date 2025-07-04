214ff41c1ecd7bef65581869e72032d07eaea8d8
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-07-04 20:58:46 +0530

    HouseRobber2 implemented

--- Revision/HouseRobber2.java
+++ Revision/HouseRobber2.java
@@ -1,8 +1,15 @@
 package Revision;
 
 public class HouseRobber2 {
-    public int rob(int[] nums) {
+    public int helper(int[] nums, int ind, int endIndex){
+        if (ind >= endIndex){
+            return 0;
+        }
 
+        return Math.max(nums[ind] + helper(nums, ind + 2, endIndex), helper(nums, ind + 1, endIndex));
+    }
+    public int rob(int[] nums) {
+        return Math.max(helper(nums, 0, nums.length - 2), helper(nums, 1, nums.length - 1));
     }
     public static void main(String[] args) {
 
