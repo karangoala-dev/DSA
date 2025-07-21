137da98128e9353df246b5be7a7896b036c57455
Karan Goala <68435889+karangoala-dev@users.noreply.github.com>
2025-07-21 22:48:50 +0530

    MaximumProductSubarray implemented

--- Striver150/FindMinimumInRotatedSortedArray.java
+++ Striver150/FindMinimumInRotatedSortedArray.java
@@ -2,7 +2,20 @@ package Striver150;
 
 public class FindMinimumInRotatedSortedArray {
     public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        int res = Integer.MAX_VALUE;
+        while (l <= r){
+            int mid = (l + ((r - l) / 2));
+            res = Math.min(nums[mid], res);
 
+            if(nums[r] < nums[mid]){
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return res;
     }
     public static void main(String[] args) {
 
